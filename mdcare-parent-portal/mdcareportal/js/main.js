f97b9cf9d2532b39c894c23eeb955e1cf9079d18
Ashokpanja <ashok.panja@vision2k.com>
2025-12-26 17:29:47 +0530

    new
    new

--- mdcare-parent-portal/mdcareportal/js/main.js
+++ mdcare-parent-portal/mdcareportal/js/main.js
@@ -12,8 +12,7 @@
     spinner();
     
     
-    // Initiate the wowjs
-    new WOW().init();
+  
 
 
     // Sticky Navbar
@@ -58,53 +57,10 @@
     });
 
 
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
+   
 
 
-    // Donation progress
-    $('.donation-item .donation-progress').waypoint(function () {
-        $('.donation-item .progress .progress-bar').each(function () {
-            $(this).css("height", $(this).attr("aria-valuenow") + '%');
-        });
-    }, {offset: '80%'});
 
-
-    // Header carousel
-    $(".header-carousel").owlCarousel({
-        animateOut: 'rotateOutUpRight',
-        animateIn: 'rotateInDownLeft',
-        items: 1,
-        autoplay: true,
-        smartSpeed: 1000,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ]
-    });
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        items: 1,
-        autoplay: true,
-        smartSpeed: 1000,
-        animateIn: 'fadeIn',
-        animateOut: 'fadeOut',
-        dots: false,
-        loop: true,
-        nav: true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ]
-    });
 
     
 })(jQuery);
